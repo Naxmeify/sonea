34a62da4edd2b1482d4ddebe2b45393b0bed6d1e
Nax (aka Matt) <m.neundorf@live.de>
2015-08-31 13:07:34 +0000

    removed docs, renamed examples

---
+++ examples/enhanced/application.js
@@ -0,0 +1,15 @@
+/**
+ * This file is only needed for direct start like: 
+ *      node application.js
+ * 
+ * Normally a sonea application is started via: 
+ *      sonea server
+ *      or
+ *      sonea s
+ */
+/*jslint node: true */
+'use strict';
+
+/*jslint nomen: true*/
+require('../../').run(__dirname);
+/*jslint nomen: false*/
