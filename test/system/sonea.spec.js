c32e6654eeff38fc5d9e2a7e4970f549f9eb2e6c
[  ] <blank.nivil@gmail.com>
2015-09-14 15:16:57 +0000

    example project name as specified in manifest, test for sonea target validation

--- test/system/sonea.spec.js
+++ test/system/sonea.spec.js
@@ -1,11 +1,19 @@
 var Sonea = global.Sonea;
 
 describe("Sonea", function() {
-   it("should a function", function() {
-     Sonea.should.be.a.Function;
-   });
+    it("should be a Function", function() {
+        Sonea.should.be.a.Function;
+    });
 
-    it("should load", function() {
-       new Sonea();
+    it("shoudl throw an error if target is not a sonea application", function() {
+        (function() {
+            new Sonea(__dirname);
+        }).should.throw;
+    });
+
+    it("should throw an error if no target specified", function() {
+        (function() {
+            new Sonea();
+        }).should.throw;
     });
 });
