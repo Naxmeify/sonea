261917e817fe0dd60168e7a15b8f490bbc4ad17b
[  ] <blank.nivil@gmail.com>
2015-09-25 13:09:26 +0000

    test spec changes

--- test/cli/commands/test.spec.js
+++ test/cli/commands/test.spec.js
@@ -1,4 +1,12 @@
+var SoneaCLICall = global.SoneaCLICall;
+
 describe("SoneaCLI Command test", function() {
+    it("should respond exit code 0", function(done) {
+        SoneaCLICall(['', '', 'test'], function(code) {
+            code.should.be.eql(0);
+            done();
+        });
+    });
     it("should run tests if current cwd does contain a sonea project or app");
     it("should not run tests if current cwd does not contain a sonea project or app");
 });
