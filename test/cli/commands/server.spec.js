f3538d1cc2e23c73d3a364fbf3cf013d7b4c9ef3
[  ] <blank.nivil@gmail.com>
2015-09-25 13:10:39 +0000

    cli cmd server spec fix of case

--- test/cli/commands/server.spec.js
+++ test/cli/commands/server.spec.js
@@ -1,4 +1,12 @@
-describe("SoneaCLI Command server", function() {
+var SoneaCLICall = global.SoneaCLICall;
+
+describe.only("SoneaCLI Command server", function() {
+    it("should respond exit code 1 for no sonea application", function(done) {
+        SoneaCLICall(['', '', 'server'], function(code) {
+            code.should.be.eql(1);
+            done();
+        });
+    });
     it("should run server if current cwd does contain a sonea project or app");
     it("should not run server if current cwd does not contain a sonea project or app");
 });
